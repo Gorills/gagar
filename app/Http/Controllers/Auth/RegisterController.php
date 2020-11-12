94dcad48f18458372ed1c7e0c687585feaeb9ea2
Gorills <gorivanickiy@gmail.com>
2020-11-12 09:58:12 +0700

    register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -1,13 +1,13 @@
 <?php
 
-// namespace App\Http\Controllers\Auth;
+namespace App\Http\Controllers\Auth;
 
-// use App\Http\Controllers\Controller;
-// use App\Providers\RouteServiceProvider;
-// use App\User;
-// use Illuminate\Foundation\Auth\RegistersUsers;
-// use Illuminate\Support\Facades\Hash;
-// use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\Controller;
+use App\Providers\RouteServiceProvider;
+use App\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
